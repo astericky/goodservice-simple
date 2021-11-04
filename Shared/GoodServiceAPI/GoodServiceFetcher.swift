de74279905fe6afb63c5634351b83efb05976f60
Chris Sanders <astericky@gmail.com>
2021-11-03 23:14:43 -0400

    add mock api code and response

--- Shared/GoodServiceAPI/GoodServiceFetcher.swift
+++ Shared/GoodServiceAPI/GoodServiceFetcher.swift
@@ -9,26 +9,27 @@ import Foundation
 import Combine
 
 protocol GoodServiceFetchable {
-  func getRoutes() -> AnyPublisher<RoutesResponse, GoodServiceError>
-  func getStops() -> AnyPublisher<StopsResponse, GoodServiceError>
+    func getRoutes() -> AnyPublisher<RoutesResponse, GoodServiceError>
+    func getStops() -> AnyPublisher<StopsResponse, GoodServiceError>
 }
 
 class GoodServiceFetcher {
-  private let session: URLSession
-  
-  init(session: URLSession = .shared) {
-    self.session = session
-  }
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
 }
 
-extension GoodServiceFetcher: GoodServiceFetchable {
-    static let routesURLDev = load("routes.json")
-    static let stopsURLDev = load("stops.json")
-    
+extension GoodServiceFetcher: GoodServiceFetchable {    
     static let routesURL = "https://goodservice.io/api/routes"
     static let stopsURL = "https://goodservice.io/api/stops"
     
     func getRoutes() -> <AnyPublisher, GoodServiceError> {
+#if DEBUG
+        let routes: RoutesResponse = load("routes.json")
+        return routes
+#else
         guard let url = URL(string: GoodServiceFetcher.routesURL) else {
             let error = GoodServiceError.network(description: "Couldn't create url.")
             return Fail(error: error).eraseToAnyPublisher()
@@ -36,13 +37,18 @@ extension GoodServiceFetcher: GoodServiceFetchable {
         return session.dataTaskPublisher(for: URLRequest(url: url))
             .decode(type: RoutesResponse.self, decoder: JSONDecoder())
             .mapError { error in
-              print(error)
-              return .network(description: error.localizedDescription)
-          }
-          .eraseToAnyPublisher()
+                print(error)
+                return .network(description: error.localizedDescription)
+            }
+            .eraseToAnyPublisher()
+#endif
     }
     
     func getStops() -> <AnyPublisher, GoodServiceError> {
+#if DEBUG
+        let stops: StopsResponse = load("stops.json")
+        return stops
+#else
         guard let url = URL(string: GoodServiceFetcher.stopsURL) else {
             let error = GoodServiceError.network(description: "Couldn't create url.")
             return Fail(error: error).eraseToAnyPublisher()
@@ -50,9 +56,10 @@ extension GoodServiceFetcher: GoodServiceFetchable {
         return session.dataTaskPublisher(for: URLRequest(url: url))
             .decode(type: StopsResponse.self, decoder: JSONDecoder())
             .mapError { error in
-              print(error)
-              return .network(description: error.localizedDescription)
-          }
-          .eraseToAnyPublisher()
+                print(error)
+                return .network(description: error.localizedDescription)
+            }
+            .eraseToAnyPublisher()
+#endif
     }
 }
