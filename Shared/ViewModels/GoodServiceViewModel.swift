57645c6b3c25dfd198b5497e7b8a9150db3cba23
Chris Sanders <astericky@gmail.com>
2021-12-09 17:22:47 -0500

    get routes by status

--- Shared/ViewModels/GoodServiceViewModel.swift
+++ Shared/ViewModels/GoodServiceViewModel.swift
@@ -12,22 +12,22 @@ import SwiftUI
 final class GoodServiceViewModel: ObservableObject, Identifiable {
     private var timestamp = ""
     
-//    private var timer: Timer?
+    //    private var timer: Timer?
     
     private var goodServiceFetcher: GoodServiceFetcher
     private var disposables = Set<AnyCancellable>()
     
     @Published var routes = [RouteViewModel]()
-    @Published var routesByStatus = [RouteViewModel]()
+    @Published var routesByStatus = [String: [RouteViewModel]]()
     
     init(goodServiceFetcher: GoodServiceFetcher,
-        scheduler: DispatchQueue = DispatchQueue(label: "GoodServiceViewModel")) {
+         scheduler: DispatchQueue = DispatchQueue(label: "GoodServiceViewModel")) {
         self.goodServiceFetcher = goodServiceFetcher
-        #if DEBUG
-            fetchRoutesFromLocalData()
-        #else
-            fetchRoutesFromAPI()
-        #endif
+#if DEBUG
+        fetchRoutesFromLocalData()
+#else
+        fetchRoutesFromAPI()
+#endif
     }
     
     func fetchRoutesFromAPI() {
@@ -37,10 +37,11 @@ final class GoodServiceViewModel: ObservableObject, Identifiable {
             .sink(
                 receiveCompletion: { _ in },
                 receiveValue: { [weak self] data in
-                guard let self = self else { return }
-                let newRoutes = data.routes.values.map(RouteViewModel.init)
-                self.routes = newRoutes
-            })
+                    guard let self = self else { return }
+                    let newRoutes = data.routes.values.map(RouteViewModel.init)
+                    self.routes = newRoutes
+                    self.routesByStatus = self.getRoutesByStatus()
+                })
             .store(in: &disposables)
     }
     
@@ -51,10 +52,11 @@ final class GoodServiceViewModel: ObservableObject, Identifiable {
             .sink(
                 receiveCompletion: { _ in },
                 receiveValue: { [weak self] data in
-                guard let self = self else { return }
-                let newRoutes = data.routes.values.map(RouteViewModel.init)
-                self.routes = newRoutes
-            })
+                    guard let self = self else { return }
+                    let newRoutes = data.routes.values.map(RouteViewModel.init)
+                    self.routes = newRoutes
+                    self.routesByStatus = self.getRoutesByStatus()
+                })
             .store(in: &disposables)
     }
     
